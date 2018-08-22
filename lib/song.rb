36bd341cabec37acbcf0898c04a71543f8beaa15
Jae Yong Kim <kim900180@gmail.com>
2018-08-22 15:50:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,9 +3,10 @@ class Song
 
   @@all = []
 
-  def initialize(name, artist = nil)
+  def initialize(name, artist = nil, genre = nil)
     @name = name
     self.artist = artist if artist
+    self.genre = genre if genre
   end
 
   def save
