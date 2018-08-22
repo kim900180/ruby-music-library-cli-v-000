a87c75cc1dc3e1c22183d037624ebc348c1a688a
Jae Yong Kim <kim900180@gmail.com>
2018-08-22 16:16:24 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,7 +43,7 @@ class Song
     all.detect{ |song| song.name == name }
   end
 
-  def self.find_by_name(name)
-    all.detect{ |song| song.name == name }
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
   end
 end
